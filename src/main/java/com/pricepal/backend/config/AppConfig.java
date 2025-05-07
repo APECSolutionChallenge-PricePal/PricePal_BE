550132e68a99f282f541946ed5d1290625bd6f20
sinyoung6491 <sinyoung6491@gmail.com>
2025-05-07 14:42:18 +0900

    change

--- src/main/java/com/pricepal/backend/config/AppConfig.java
+++ src/main/java/com/pricepal/backend/config/AppConfig.java
@@ -1,2 +1,14 @@
-package com.pricepal.backend.config;public class AppConfig {
+package com.pricepal.backend.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
